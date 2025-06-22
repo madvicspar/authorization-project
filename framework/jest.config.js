c3010fd112398188537ed22f30ef1a88c3212c98
madvicspar <viktoria.akkuzina@yandex.ru>
2025-06-23 00:02:14 +0500

    refactor: удалены лишние файлы

--- framework/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jest-environment-jsdom',
-    setupFilesAfterEnv: ['@testing-library/jest-dom'],
-    moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/src/$1'
-    },
-    testMatch: ['**/src/**/*.test.ts']
-};
